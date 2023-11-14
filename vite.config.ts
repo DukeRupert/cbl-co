3f5c22a568334bf61b292bfe05506a652800f694
Logan <logan@fireflysoftware.dev>
2023-11-14 08:14:37 -0700

    Switched to vercel adapter. Bugfix: Removed import of svelte-img in configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { imagetools } from '@zerodevx/svelte-img/vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
 
 export default defineConfig({
